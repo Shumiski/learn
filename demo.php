6fe737d1e87a3cb4fa5c3924649c2c5186e372cb
Shumiski <advan@rozmowa.nu>
2015-09-08 16:32:44 -0300

    adding values to the property

--- demo.php
+++ demo.php
@@ -8,5 +8,13 @@ $address = new Address;
 echo "<h2>Empty Address</h2>";
 echo "<tt><pre>" . var_export($address, TRUE) . "</tt></pre>";
 
+echo "<h2>Setting properties</h2>";
+$address->streetAddress1 = "Rua Tal, 1070";
+$address->cityName = "São Paulo";
+$address->subdivisionName = "SP";
+$address->postalCode = "12345-123";
+$address->country = "Brazil";
+echo "<tt><pre>" . var_export($address, TRUE) . "</tt></pre>";
+
 
 ?>
